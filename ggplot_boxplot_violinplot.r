22863087cfdb28232cb9d20988595f72edc4282f
dastangoo <hamed.dastangoo>
2017-03-27 18:11:20 +0430

    Commit for ggplot_boxplot_violinplot.r file.

---
+++ ggplot_boxplot_violinplot.r
@@ -0,0 +1,7 @@
+ggplot(diamonds, aes(y= carat, x = 1)) + geom_boxplot()
+ggplot(diamonds, aes(y= carat, x = cut)) + geom_boxplot()
+ggplot(diamonds, aes(y= carat, x = cut)) + geom_violin()
+g <- ggplot(diamonds, aes(y = carat, x = cut))
+g + geom_point() + geom_violin()
+g + geom_violin() + geom_point()
+g + geom_jitter() + geom_violin()
