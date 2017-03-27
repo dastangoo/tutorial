4d43aa5cac6f4117adcf91cfc5516feb60ef6b0e
dastangoo <hamed.dastangoo>
2017-03-27 17:57:36 +0430

    Commit for ggplot_scatterplot.r

---
+++ ggplot_scatterplot.r
@@ -0,0 +1,7 @@
+require(ggplot2)
+ggplot(diamonds, aes(x = carat, y = price)) + geom_point()
+g <- ggplot(diamonds, aes(x = carat, y = price))
+g + geom_point()
+g + geom_point(aes(color = color))
+g + geom_point(aes(color = color, shape = clarity))
+g + geom_point(aes(color = color, shape = cut))
