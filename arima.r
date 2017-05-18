f2dd32cf164a0e98925f5beaa53c1f8e4f07c4a8
dastangoo <hamed.dastangoo>
2017-05-18 21:36:01 +0430

    ARIMA

---
+++ arima.r
@@ -0,0 +1,10 @@
+install.packages("forecast")
+require(forecast)
+usBest <- auto.arima(x=us) 
+acf(usBest$residuals) 
+pacf(usBest$residuals) 
+coef(usBest) 
+predict(usBest, n.ahead=5, se.fit=TRUE) 
+theForecast <- forecast(object=usBest, h=5) 
+plot(theForecast) 
+
