c8d30c3bd56433abe202e9daff862494f3a1e3f1
dastangoo <hamed.dastangoo>
2017-05-19 15:00:08 +0430

    Reporting

---
+++ Sample/Simple/R/Multiplication.r
@@ -0,0 +1,17 @@
+#' @title simple.ex
+#' @describeIn Simple Example
+#' @details This is a simple example of function
+#' @aliases simple.ex
+#' @author Hamed Dastangoo
+#' @export simple.ex
+#' @param x A numeric vector
+#' @param y A numeric vector
+#' @return x times y
+#' @examples
+#' simple.ex(5, 3)
+#' simple.ex(1:3, 4:6)
+#'
+simple.ex <- function(x, y)
+{
+  return(x * y)
+}
